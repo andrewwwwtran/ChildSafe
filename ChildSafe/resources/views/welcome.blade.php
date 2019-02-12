19a08c256a9769835e057d65c49a91364c4e3e35
andrew <andrewtran912@gmail.com>
2019-02-12 00:48:55 -0800

    test

--- ChildSafe/resources/views/welcome.blade.php
+++ ChildSafe/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    ChildSafe Website
                 </div>
 
                 <div class="links">
