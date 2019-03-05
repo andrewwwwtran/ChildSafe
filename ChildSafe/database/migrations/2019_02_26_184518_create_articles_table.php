d5de5d08cef9606e459223b0376a7299a1130b50
andrew <andrewtran912@gmail.com>
2019-03-05 15:44:08 -0800

    test

--- ChildSafe/database/migrations/2019_02_26_184518_create_articles_table.php
+++ ChildSafe/database/migrations/2019_02_26_184518_create_articles_table.php
@@ -15,7 +15,7 @@ class CreateArticlesTable extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('title');
+            $table->timestamps();
             $table->text('body');
             $table->timestamps();
         });
